713f778e5255926b42ee592797c3cac24de8aad2
marshl <marshl@live.com.au>
2016-05-05 21:33:36 +1000

    Switched from OptionParser to argparse;

--- freeman/freeman.py
+++ freeman/freeman.py
@@ -1,37 +1,32 @@
-from optparse import OptionParser
+#!/usr/bin/env python2
+import argparse
+import CX_Oracle
 
-parser = OptionParser()
-parser.add_option("-u", "--username", dest="username", default="xviewmgr",
+parser = argparse.ArgumentParser()
+parser.add_argument("--username", default="xviewmgr",
                   help="the user to connect as")
 
-parser.add_option("-p", "--password", dest="password",
-                  help="the password of the user")
+parser.add_argument("--password", help="the password of the user")
 
-parser.add_option("-r", "--port", dest="port", default="1521",
+parser.add_argument("--port", default="1521",
                   help="the port to connect to")
 
-parser.add_option("-o", "--host", dest="host",
-                  help="the host to connect to")
+parser.add_argument("--host", help="the host to connect to")
 
-parser.add_option("-s", "--sid", dest="sid",
-                  help="the Oracle System ID to connect to")
+parser.add_argument("--sid", help="the Oracle System ID to connect to")
 
-parser.add_option("-d", "--directory", dest="directory",
-                  help="the directory to compare with the database")
-
-(options, args) = parser.parse_args()
-
-if len(args) != 1:
-    parser.error("incorrect number of arguments")
-
-import argparse
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('username', metavar='u', type=string, nargs='+',
-                   help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                   const=sum, default=max,
-                   help='sum the integers (default: find the max)')
+parser.add_argument("--directory", help="the CodeSource directory to compare with the database")
 
 args = parser.parse_args()
-print( args.accumulate(args.integers) )
+
+class FolderDefinition:
+    def __init__(self, directory, extension, loadstatement):
+        self.directory = directory;
+        self.extension = extension;
+        self.loadstatement = loadstatement;
+
+folder_definitions = [ FolderDefinition("ResourceTypes", "*.xml",
+"""SELECT rt.xml_data.getClobVal()
+FROM decmgr.resource_types rt
+WHERE rt.res_type = REPLACE( :file_name, '.xml', '' )""") ]
+
