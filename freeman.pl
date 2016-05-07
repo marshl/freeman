e29c50ff386b98b25d03a3936a529fbd0e073461
marshl <marshl@live.com.au>
2016-05-07 14:55:18 +1000

    Fixed some PerlCritic issues;

--- freeman.pl
+++ freeman.pl
@@ -18,18 +18,12 @@ use File::Spec;
 use File::Temp qw(tempfile);
 use Getopt::Long;
 
-my @clobTypes = getClobTypes();
+my @directory_types = get_directory_types();
 
-die "Usage: clobcheck.pl code_source_folder host port sid username password\n" unless @ARGV == 6;
+if ( @ARGV != 6 ) {
+    die "Usage: freeman.pl code_source_folder host port sid username password\n";
+}
 my ( $code_source_folder, $host, $port, $sid, $username, $password ) = @ARGV;
-# GetOptions(
-    # 'folder=s' => \$code_source_folder,
-    # 'host=s' => \$host,
-    # 'port=s' => \$port,
-    # 'sid=s' => \$sid,
-    # 'username=s' => \$username,
-    # 'password=s' => \$password
-# );
 
 die "Invalid folder $code_source_folder" if not -d $code_source_folder;
 
@@ -37,41 +31,35 @@ my $dbh = DBI->connect( "dbi:Oracle:host=$host;port=$port;sid=$sid;", $username,
 # Expand the read length to a safe size
 $dbh->{LongReadLen} = 512 * 1024;
 
-CLOBTYPE:
-foreach my $clobType ( @clobTypes ) {
-    print "\nComparing $clobType->{'name'}\n";
+DIRECTORY_TYPE:
+foreach my $directory_type ( @directory_types ) {
+    print "\nComparing $directory_type->{'name'}\n";
 
-    # my $directoryPath = $code_source_folder . '\\' . $clobType->{'dir'};
-    my $directoryPath = File::Spec->catfile( $code_source_folder, $clobType->{'dir'} );
+    my $directory_path = File::Spec->catfile( $code_source_folder, $directory_type->{'directory'} );
 
-    if ( not -d $directoryPath ) {
-        print "Directory not found: $directoryPath\n";
-        next CLOBTYPE;
+    if ( not -d $directory_path ) {
+        print "Directory not found: $directory_path\n";
+        next DIRECTORY_TYPE;
     }
-    #    opendir(D, $fullpath ) or die "Can't open directory $fullpath: $!";
 
-    # my @fileList = grep !/^\.\.?$/, readdir(D);
-
-    my $statement = $dbh->prepare( $clobType->{'stmt'} ) or die $dbh->errstr;
-    print Dumper($clobType);
-    my @fileList;# = File::Find->file->name( $clobType->{'extension'} )->in($directoryPath);
+    my $statement = $dbh->prepare( $directory_type->{'statement'} ) or die $dbh->errstr;
+    #print Dumper($directory_type);
+    my @file_list;
     find(
         sub {
-            if ( m/^.+?$clobType->{'extension'}$/ ) {
-                push @fileList, $File::Find::name;
+            if ( m/^.+?$directory_type->{'extension'}$/xs ) {
+                push @file_list, $File::Find::name;
             }
         },
-        $directoryPath
+        $directory_path
     );
-    #print Dumper(\@fileList);
 
     FULLPATH:
-    foreach my $fullpath ( @fileList ) {
+    foreach my $fullpath ( @file_list ) {
         my $filename = basename($fullpath);
-        #(my $without_extension = $filename) =~ s/\.[^.]*$//;
-        my ($without_extension) = fileparse($fullpath, qr/\.[^.]*/);
+        my ($without_extension) = fileparse($fullpath, qr/[.][^.]*/);
 
-        print "$without_extension\n";
+        #print "$without_extension\n";
 
         $statement->execute($without_extension) or die $statement->errstr();
         my $filedata = $statement->fetchrow();
@@ -81,9 +69,8 @@ foreach my $clobType ( @clobTypes ) {
             next FULLPATH;
         }
 
-        #die "No data found." if not defined $data and not defined $bindata;
         if ( $statement->rows == 0 ) {
-            print "No data found for $filename\n";
+            print "New File: $filename\n";
             next FULLPATH;
         }
 
@@ -92,131 +79,136 @@ foreach my $clobType ( @clobTypes ) {
             open ( my $fh, '<', $fullpath ) or die "Could not open file $fullpath: $OS_ERROR";
             local $INPUT_RECORD_SEPARATOR = undef;
             $local_file_content = <$fh>;
+            close $fh;
         }
 
-        my $patterns = $clobType->{'remove_patterns'};
+        my $patterns = $directory_type->{'remove_patterns'};
         foreach my $pattern ( @{$patterns} ) {
             $local_file_content =~ s/$pattern//g;
             $filedata =~ s/$pattern//g;
         }
-        #}
 
         if ( $local_file_content ne $filedata ) {
-            print "File mismatch $filename\n";
+            print "Modified: $filename\n";
         }
     }
 }
 
 $dbh->disconnect();
 
-sub getClobTypes {
+sub get_directory_types {
+
+    my $whitespace_regex = '\s';
+    my $js_regex = '\/[*].+?[*]\/';
+    my $xml_regex = '<!--.\*-->';
+
     return (
     {
         name => 'Fox5Modules - JavaScript',
-        dir => 'Fox5Modules',
+        directory => 'Fox5Modules',
         extension => '\.js',
-        remove_patterns => [ '\s', '\/\*.+?\*\/' ],
-        stmt => "
-SELECT data
+        remove_patterns => [ $whitespace_regex, $js_regex ],
+        statement => 
+"SELECT data
 FROM envmgr.fox_components_fox5 fc5
 WHERE fc5.name = 'js/' || ?",
     },
 
     {
         name => 'Fox5Modules - Modules',
-        dir => 'Fox5Modules',
+        directory => 'Fox5Modules',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT data
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT data
 FROM envmgr.fox_components_fox5 fc5
 WHERE fc5.name = ?",
     },
 
     {
         name => 'xview',
-        dir => 'XviewDefinitions',
+        directory => 'XviewDefinitions',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT x.xview_metadata.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT x.xview_metadata.getClobVal()
 FROM xviewmgr.xview_definition_metadata x
 WHERE x.file_name = ? || '.xml'",
     },
 
     {
         name => 'Xview 2s',
-        dir => 'Xview2Definitions',
+        directory => 'Xview2Definitions',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT x.xview_metadata_formatted
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT x.xview_metadata_formatted
 FROM xviewmgr.xview2_definition_metadata x
 WHERE x.file_name = ? || '.xml'",
     },
 
     {
-        name => 'Xviews',
-        dir => 'NavBarActionGroups\\',
+        name => 'NavBar Action Groups',
+        directory => 'NavBarActionGroups',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT x.xml_data.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT x.xml_data.getClobVal()
 FROM envmgr.nav_bar_action_groups x
 WHERE x.mnem = ?",
     },
 
     {
         name => 'Navbar Categories',
-        dir => 'NavBarActionCategories\\',
+        directory => 'NavBarActionCategories',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT x.xml_data.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT x.xml_data.getClobVal()
 FROM envmgr.nav_bar_action_categories x
 WHERE x.mnem = ?",
     },
 
     {
         name => 'DocLib Types',
-        dir => 'DocLibTypes',
+        directory => 'DocLibTypes',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT dlt.xml_data.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT dlt.xml_data.getClobVal()
 FROM doclibmgr.document_library_types dlt
 WHERE dlt.document_library_type = ?",
     },
 
     {
         name => 'Document Templates',
-        dir => 'DocumentTemplates',
+        directory => 'DocumentTemplates',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT dt.xml_data.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT dt.xml_data.getClobVal()
 FROM decmgr.document_templates dt
 WHERE dt.name = ?",
     },
 
     {
         name => 'File Folder Types',
-        dir => 'FileFolderTypes',
+        directory => 'FileFolderTypes',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT fft.xml_data.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT fft.xml_data.getClobVal()
 FROM decmgr.file_folder_types fft
 WHERE fft.file_folder_type = ?",
     },
 
     {
         name => 'Mapsets',
-        dir => 'Mapsets',
+        directory => 'Mapsets',
         extension => '\.xml',
-        remove_patterns => [ '\s', '<!--.\*-->' ],
-        stmt => "
-SELECT emm.metadata.getClobVal()
+        remove_patterns => [ $whitespace_regex, $xml_regex ],
+        statement => 
+"SELECT emm.metadata.getClobVal()
 FROM envmgr.env_mapsets_metadata emm
 WHERE emm.domain = ?",
     },
